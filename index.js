303713c7b34d93c3f5d5803f2743610daf2f56ba
srinivasrk <srinivasrk92@gmail.com>
2018-09-12 09:55:17 -0700

    add express support

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const express = require('express')
+const app = express()
+
+
 var clientConfig = {
     bucketUri: 's3://aws-athena-query-results-139643316571-us-east-1'
 }
@@ -11,9 +15,16 @@ var awsConfig = {
 var athena = require("athena-client")
 var client = athena.createClient(clientConfig, awsConfig)
 
-client.execute('select * from parquet.device_data limit 10', function(err, data) {
-    if (err) {
-        return console.error(err)
-    }
-    console.log(data)
+
+app.get('/', (req, res) => {
+  console.log("Access / " + " Query : " + req.headers.query)
+  let query = req.headers.query;
+  client.execute(query, function(err, data) {
+      if (err) {
+          return(err)
+      }
+      res.json(data)
+  })
 })
+
+app.listen(3300, () => console.log('Example app listening on port 3300!'))
